774b19f9b6b7bba223d8ca0290ccb49cca2556fc
marcusm117 <marcusmin117@gmail.com>
2024-04-30 11:39:19 -0400

    update run_identity_chain.sh

--- examples/run_identity_chain.sh
+++ examples/run_identity_chain.sh
@@ -14,7 +14,7 @@ export IDENTITY_CHAIN_HOME=YOUR_OWN_PATH/IdentityChain  # no / at the end
 
 # for open-source models from HuggingFace, when using greedy, add the flag --greedy_early_stop to accelerate
 # for OpenAI models, don't use --greedy_early_stop!!! temperature = 0 is NOT greedy!!!
-# for EvalPlus-Mini-v0.1.6_reformatted.jsonl, use --resume_task_bs 1, since HumanEval/0 is used for prompt
+# for EvalPlus-Mini-v0.1.6_reformatted.jsonl (or other versions), use --resume_task_bs 1, since HumanEval/0 is used for prompt
 # for MBPP-S_test_reformatted.jsonl, use --resume_task_bs 0, since there's a separate prompt split
 
 for MODEL in "bigcode/starcoderbase-1b"  # feel free to add other supported models
