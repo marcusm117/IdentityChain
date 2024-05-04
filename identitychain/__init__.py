c8f72adfdcd84576020d9a98f8ab2da7cdb12a4b
marcusm117 <marcusmin117@gmail.com>
2024-05-04 01:09:29 -0400

    Bump version: 0.0.1 → 0.1.0

--- identitychain/__init__.py
+++ identitychain/__init__.py
@@ -6,5 +6,5 @@ from .executor import unsafe_execute
 from .identity_chain import IdentityChain, IdentityChainError, INSTRUCTION_MODELS, FOUNDATION_MODELS
 
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __all__ = ["unsafe_execute", "IdentityChain", "IdentityChainError", "INSTRUCTION_MODELS", "FOUNDATION_MODELS"]
